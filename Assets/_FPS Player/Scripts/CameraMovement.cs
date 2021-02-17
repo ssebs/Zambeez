7226ce43ae7d296f27cd7a6e06d8a999657d8f13
Sebastian Safari <ssebs@ymail.com>
2021-02-16 17:26:28 -0800

    initial

---
+++ Assets/_FPS Player/Scripts/CameraMovement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    PlayerInput playerInput;
+
+    Vector2 _mouseAbsolute;
+    Vector2 _smoothMouse;
+
+    public GameObject characterBody;
+    public GunController gunController;
+
+    [SerializeField]
+    private Vector2 clampInDegrees = new Vector2(360, 180);
+    [SerializeField]
+    private Vector2 sensitivity = new Vector2(2, 2);
+    [SerializeField]
+    private Vector2 smoothing = new Vector2(3, 3);
+    [SerializeField]
+    private Vector2 targetDirection;
+    [SerializeField]
+    private Vector2 targetCharacterDirection;
+    [SerializeField]
+    private Vector2 ADSSensitivity = new Vector2(0.5f, 0.5f);
+
+
+    void Start()
+    {
+        // Set target direction to the camera's initial orientation.
+        targetDirection = transform.localRotation.eulerAngles;
+
+        // Set target direction for the character body to its inital state.
+        if (characterBody)
+        {
+            targetCharacterDirection = characterBody.transform.localRotation.eulerAngles;
+            playerInput = characterBody.GetComponent<PlayerInput>();
+        }
+
+    }
+
+    void Update()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        // Allow the script to clamp based on a desired target value.
+        var targetOrientation = Quaternion.Euler(targetDirection);
+        var targetCharacterOrientation = Quaternion.Euler(targetCharacterDirection);
+
+        // Get raw mouse input for a cleaner reading on more sensitive mice.
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        // Scale input against the sensitivity setting and multiply that against the smoothing value.
+
+        if (characterBody && playerInput.aiming)
+        {
+            if (gunController)
+            {
+                if (gunController.SelectedGun().gun.aimFOV < 40)
+                {
+                    Vector2 slowADSSensitivity = new Vector2(ADSSensitivity.x * 0.75f, ADSSensitivity.y * 0.75f);
+                    mouseDelta = Vector2.Scale(mouseDelta, new Vector2(slowADSSensitivity.x * smoothing.x, slowADSSensitivity.y * smoothing.y));
+                }
+            }
+            else
+            {
+                mouseDelta = Vector2.Scale(mouseDelta, new Vector2(ADSSensitivity.x * smoothing.x, ADSSensitivity.y * smoothing.y));
+            }
+        }
+        else
+        {
+            mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+        }
+
+        // Interpolate mouse movement over time to apply smoothing delta.
+        _smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+        _smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+
+        // Find the absolute mouse movement value from point zero.
+        _mouseAbsolute += _smoothMouse;
+        // Clamp and apply the local x value first, so as not to be affected by world transforms.
+        if (clampInDegrees.x < 360)
+            _mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+
+        // Then clamp and apply the global y value.
+        if (clampInDegrees.y < 360)
+            _mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+
+        transform.localRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
+
+        // If there's a character body that acts as a parent to the camera
+        if (characterBody)
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
+            characterBody.transform.localRotation = yRotation * targetCharacterOrientation;
+        }
+        else
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
+            transform.localRotation *= yRotation;
+        }
+    }
+
+    public void AddRecoil(Vector3 recoil, float time)
+    {
+        float recoilElapsed = 0;
+        StartCoroutine(recoilIncrease());
+        IEnumerator recoilIncrease()
+        {
+            while (recoilElapsed < time)
+            {
+                recoilElapsed += Time.deltaTime;
+                _mouseAbsolute += (Vector2)(recoil * Time.deltaTime / time);
+                yield return null;
+            }
+        }
+    }
+}
